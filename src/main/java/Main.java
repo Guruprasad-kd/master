1770b22309e7560cb84639cf823b709025fd97e3
Guruprasad-kd <Phoenixgamer682@gmail.com>
2025-07-05 13:20:44 +0530

    i have made my first code

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,13 +1,30 @@
-import java.awt.*;
-import java.util.Arrays;
+import java.text.NumberFormat;
+import java.util.Locale;
+import java.util.Scanner;
 
-public class Main{
-    public static void main(String [] args) {
-        int [] numbers = {2,3,52,41,41};
-        Arrays.sort(numbers);
-        //System.out.println(numbers.length);
+public class Main {
+    public static void main(String[] args) {
+        final byte MONTHS_IN_YEAR = 12;
+        final byte PERCENT = 100;
 
-        System.out.println( Arrays.toString(numbers));
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter the principal:");
+        int principal = scanner.nextInt();
+
+        System.out.println("Enter the annual interest rate:");
+        float annualInterest = scanner.nextFloat();
+        float monthlyInterest = annualInterest / PERCENT / MONTHS_IN_YEAR;
+
+        System.out.println("Enter the period (in years):");
+        int years = scanner.nextInt();
+        int numberOfPayments = years * MONTHS_IN_YEAR;
+
+        double mortgage = principal
+                * (monthlyInterest * Math.pow(1 + monthlyInterest, numberOfPayments))
+                / (Math.pow(1 + monthlyInterest, numberOfPayments) - 1);
+        Locale locale = new Locale("en","us");
+        String mortgageFormatted = NumberFormat.getCurrencyInstance(locale).format(mortgage);
+        System.out.println("Mortgage: " + mortgageFormatted);
     }
-
 }
