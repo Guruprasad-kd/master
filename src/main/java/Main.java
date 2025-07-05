7674dcd62aec20ad2c78eb8924f15bfc67d4c4b8
Guruprasad-kd <Phoenixgamer682@gmail.com>
2025-07-05 10:38:31 +0530

    i have made my first code

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,13 @@
+import java.awt.*;
+import java.util.Arrays;
+
 public class Main{
     public static void main(String [] args) {
-        System.out.println("Hello World");
+        int [] numbers = {2,3,52,41,41};
+        Arrays.sort(numbers);
+        //System.out.println(numbers.length);
+
+        System.out.println( Arrays.toString(numbers));
     }
 
 }
